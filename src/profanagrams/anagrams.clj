b0527cc8e0825e15ad31758fc95149192d13deea
Tom Hancock <thomas.ian.hancock@gmail.com>
2014-12-05 12:21:27 -0600

    Working implementation of the recursive trie search, needs a bit of cleaning up + testing

--- src/profanagrams/anagrams.clj
+++ src/profanagrams/anagrams.clj
@@ -1,10 +1,28 @@
 (ns profanagrams.anagrams
   (:require [profanagrams.trie :refer [EOW]]))
 
+(defn get-dirty-words
+  [dirty-trie input-frequencies results]
+  (for [[c subtrie] dirty-trie]
+    (let [frequency   (get input-frequencies c)
+          terminal    (and frequency (get subtrie EOW))
+          new-freqs   (cond
+                       (not frequency) input-frequencies
+                       (= frequency 1) (dissoc input-frequencies c)
+                       :else           (assoc input-frequencies c (dec frequency)))
+          new-results (if terminal
+                        (conj results [new-freqs terminal])
+                        results)]
+      (if frequency 
+        (get-dirty-words subtrie
+                         new-freqs 
+                         new-results)
+        new-results))))
+
 (defn search
-  [trie-root input-frequencies]
-  (let [results (atom [])
-        helper (fn f [trie freqs]
+  [trie-root input-frequencies partial-result]
+  (let [results (atom #{})
+        helper (fn f [trie freqs partial]
                  (doseq [[c subtrie] trie]
                    (let [frequency   (get freqs c)
                          terminal    (and frequency (get subtrie EOW))
@@ -12,12 +30,21 @@
                                       (not frequency) freqs
                                       (= frequency 1) (dissoc freqs c)
                                       :else           (assoc freqs c (dec frequency)))]
-                     (when terminal (swap! results conj [terminal new-freqs]))
-                     (when frequency (f subtrie new-freqs)))))]
-    (helper trie-root input-frequencies)
+                     (cond
+                      (and terminal (empty? new-freqs)) (swap! results conj [:full (conj partial terminal)])
+                      terminal                          (f trie-root new-freqs (conj partial terminal))
+                      frequency                         (f subtrie new-freqs partial)
+                      :else                             (when (not-empty partial) ;; FIXME - not very efficient
+                                                          (swap! results conj [:partial partial]))))))]
+    (helper trie-root input-frequencies #{partial-result})
     @results))
 
 (defn get-anagrams
-  [trie input-characters]
-  (let [input-frequencies (frequencies input-characters)]
-    ))
+  [dirty-trie clean-trie input-characters]
+  (let [input-frequencies (frequencies input-characters)
+        dirty-results (->> (get-dirty-words dirty-trie input-frequencies [])
+                           flatten
+                           (partition 2))
+        full-results (for [[freqs partial-result] dirty-results]
+                       (search clean-trie freqs partial-result))]
+    (apply clojure.set/union full-results)))
