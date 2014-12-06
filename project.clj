e66838cbba07989c9c9bd7c97782688c6ecaba9b
Tom Hancock <thomas.ian.hancock@gmail.com>
2014-12-06 16:36:47 -0600

    Minimal front end

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring "1.3.2"]]
+                 [ring "1.3.2"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler profanagrams.core/app
          :init profanagrams.core/load-tries}
--- src/profanagrams/core.clj
+++ src/profanagrams/core.clj
@@ -4,7 +4,8 @@
             [ring.middleware.params :refer [wrap-params]]
             [clojure.string :refer [split]]
             [profanagrams.trie :refer [build-trie]]
-            [profanagrams.anagrams :refer [get-anagrams]]))
+            [profanagrams.anagrams :refer [get-anagrams]]
+            [hiccup.page :refer [html5]]))
 
 (def dirty-trie (delay (build-trie (-> "resources/badwords.dict" slurp (split #"\n")))))
 (def clean-trie (delay (build-trie (-> "resources/google-10000-english.txt" slurp (split #"\n")))))
@@ -15,7 +16,10 @@
   @clean-trie)
 
 (defroutes r
-  (GET "/" [] "<h1>Hello World</h1>")
+  (GET "/" [] (html5 [:form {:action :anagrams
+                             :method :get}
+                      [:input {:type  :search
+                               :name  :input}]]))
   (GET "/anagrams" [input] (when input (str (get-anagrams @dirty-trie @clean-trie input)))))
 
 (def app (wrap-params r))
